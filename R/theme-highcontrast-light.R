c6b8d681904ce9dc42c45d8f71142b32b5723750
Mara Averick <maraaverick@gmail.com>
2021-10-23 12:41:50 -0600

    wrap in hc_theme()

--- R/theme-highcontrast-light.R
+++ R/theme-highcontrast-light.R
@@ -11,19 +11,18 @@
 #'   hc_add_theme(hc_theme_highcontrast_light())
 #' @export
 hc_theme_highcontrast_light <- function(...) {
-  theme <- 
-    list(
-      colors = c(
-        '#5f98cf', '#434348', '#49a65e', '#f45b5b', '#708090', 
-        '#b68c51', '#397550', '#c0493d', '#4f4a7a', '#b381b3'
-        ),
-      navigator = list(
-        series = list(
-          color = '#5f98cf',
-          lineColor = '#5f98cf'
-        )
+  theme <- hc_theme(
+    colors = c(
+      '#5f98cf', '#434348', '#49a65e', '#f45b5b', '#708090', 
+      '#b68c51', '#397550', '#c0493d', '#4f4a7a', '#b381b3'
+    ),
+    navigator = list(
+      series = list(
+        color = '#5f98cf',
+        lineColor = '#5f98cf'
       )
     )
+  )
   
   theme <- structure(theme, class = "hc_theme")
   
